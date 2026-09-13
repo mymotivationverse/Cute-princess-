abead9e7164c6aa996a6d73b0180addd3d11d9da
mymotivationverse <mymotivationverse@gmail.com>
2026-09-13 23:16:55 +0530

    Modify DATABASE_URI for MongoDB connection
    Updated DATABASE_URI to include retryWrites and w parameters.

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information *
-DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://Pankaj9876:Pankaj9876@cluster0.klljsmr.mongodb.net/?appName=Cluster0')
+DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://Pankaj9876:Pankaj9876@cluster0.klljsmr.mongodb.net/Cluster0?retryWrites=true&w=majority')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'Cluster0')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
