026d39b66a1a7bfb8f16de6225772c73050fb014
mymotivationverse <mymotivationverse@gmail.com>
2026-09-15 13:02:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,3 @@
-@Client.on_message(filters.command("start") & filters.incoming)
-async def start(client, message):
-    print("🔥 START RECEIVED:", message.command)
 import os
 import logging
 import random
