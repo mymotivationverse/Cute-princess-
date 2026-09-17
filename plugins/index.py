a693b4dc0c53427fb6062f3cc465921d7e9a44e4
mymotivationverse <mymotivationverse@gmail.com>
2026-09-17 19:11:39 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -61,8 +61,8 @@ async def send_for_index(bot, message):
         if chat_id.isnumeric():
             chat_id  = int(("-100" + chat_id))
     elif message.forward_from_chat.type == enums.ChatType.CHANNEL:
-            last_msg_id = message.forward_from_message_id or message.id
-            chat_id = message.chat.id
+            last_msg_id = message.forward_from_message_id
+            chat_id = message.forward_from_chat.username or message.forward_from_chat.id
     else:
         return
     try:
